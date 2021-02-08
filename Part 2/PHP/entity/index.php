c0e154a09b9787117fd19f8f75ed433eba403ad1
mriley-wallace <mriley-wallace@users.noreply.github.com>
2021-02-08 02:02:11 +0000

    Commit 4.2 Had an issue with the last line of the RDF containing a comma that broke the JSON-LD playground visualisation

--- Part 2/PHP/entity/index.php
+++ Part 2/PHP/entity/index.php
@@ -1,26 +1,30 @@
 <?php
 $OpenTheCSV = fopen("../entity/UniResults.csv", "r");
 
-echo '{ "@context" : { "Region" : "http://schema.org", "educationCredentialAwarded" : "http://web.socem.plymouth.ac.uk" }, "total" : [ { "@type" : "Course", </br>';
+echo '{ "@context" : { "Region" : "http://schema.org", "educationCredentialAwarded" : "http://web.socem.plymouth.ac.uk" }, "total" : [ </br>';
 
 
 while (($line = fgetcsv($OpenTheCSV)) !== false) {
-    if (!empty($line)) {
-
-        if ($line == $OpenTheCSV) {
-            echo($line);
-        }
+    if ($line != NULL) {
         echo '{ </br>';
-        echo '"@type"  :  "Course" </br>';
-        echo '"educationalCredentialAwarded"  :  '.$line[0].'</br>';
-        echo '"total-type" :  '.$line[1].'</br>';
-        echo '"person"  :  '.$line[2].'</br>';
-        echo '"date"  :  '.$line[3].'</br>';
-        echo '"total"  :  '.$line[5].'</br>';
+        echo '"@type"  :  "Course", </br>';
+        echo '"educationalCredentialAwarded"  :  "' . $line[0] . '",</br>';
+        echo '"total-type" :  "' . $line[1] . '",</br>';
+        echo '"person"  :  "' . $line[2] . '",</br>';
+        echo '"date"  :  "' . $line[3] . '",</br>';
+        echo '"total"  :  "' . $line[5] . '"</br>';
+    }
+        if($line[5] != "2.5"){
+
         echo '}, </br>';
         echo '</br>';
-
+    } else{
+        echo '} </br>';
+        echo '</br>';
     }
+
 }
+echo ']}';
 fclose($OpenTheCSV);
+
 
